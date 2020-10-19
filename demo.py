92364a0fdbf18c8d7d2fa29c9c57e84eeb17245c
Noah Lipsyc <n.lipsyc@gmail.com>
2020-10-19 14:22:50 -0400

    Add better logging for the game
    Wanted to verify that:
    
    1) There was variation between the two algorithms
    2) There was variation within the algorithms
    3) The ties were not necesarrily just from identical games being played out over and over again.

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
-from runtime import Runtime
+from collections import Counter
+from dataclasses import dataclass
+
 from algorithms import random_algorithm_factory
 from game import FreeForAllGame
+from runtime import Runtime
 
 NUMBER_OF_ROUNDS = 100
 
@@ -10,22 +13,48 @@ rt = Runtime(FreeForAllGame, random_algorithm_0, random_algorithm_1)
 rt.simulate_game()
 
 
-win_record = {0: 0, 1: 0}
+@dataclass
+class GameLog:
+    """A log of the results of a single game."""
+
+    game_number: int
+    player_0_score: int
+    player_1_score: int
+
+    def __str__(self):
+        return f"Player 0 Score: {player_0_score}\nPlayer 1 Score: {player_1_score}\nWinner: {self.winner}"
+
+    @property
+    def winner(self):
+        if self.player_0_score == self.player_1_score:
+            return None
+
+        if self.player_0_score > self.player_1_score:
+            return 0
+        else:
+            return 1
+
+
+game_log_collection = []
 for game in range(NUMBER_OF_ROUNDS):
     print(f"Game {game}\n")
     player_0_score, player_1_score = rt.simulate_game()
 
-    if player_0_score > player_1_score:
-        win_record[0] += 1
+    log = GameLog(game, player_0_score, player_1_score)
+    game_log_collection.append(log)
 
-    elif player_0_score < player_1_score:
-        win_record[1] += 1
-        print("=======================\n")
+    print(log)
 
-print(
-    f"Player 0 wins {win_record[0]}\nPlayer 1 wins {win_record[1]}\nTies: {NUMBER_OF_ROUNDS - win_record[0] - win_record[1]}"
-)
-tie_percent = (NUMBER_OF_ROUNDS - win_record[0] - win_record[1]) / NUMBER_OF_ROUNDS
-print(
-    f"Player 0 wins {win_record[0] / NUMBER_OF_ROUNDS}\nPlayer 1 wins {win_record[1] / NUMBER_OF_ROUNDS}\nTies: {tie_percent}"
-)
+    print("=======================\n")
+player_0_score_counts = Counter([log.player_0_score for log in game_log_collection])
+player_1_score_counts = Counter([log.player_1_score for log in game_log_collection])
+print(f"Player 0 scores {sorted(player_0_score_counts.items())}")
+print(f"Player 1 scores {sorted(player_1_score_counts.items())}")
+print(f"Win counts{Counter([log.winner for log in game_log_collection])}")
+# print(
+#     f"Player 0 wins {win_record[0]}\nPlayer 1 wins {win_record[1]}\nTies: {NUMBER_OF_ROUNDS - win_record[0] - win_record[1]}"
+# )
+# tie_percent = (NUMBER_OF_ROUNDS - win_record[0] - win_record[1]) / NUMBER_OF_ROUNDS
+# print(
+#     f"Player 0 wins {win_record[0] / NUMBER_OF_ROUNDS}\nPlayer 1 wins {win_record[1] / NUMBER_OF_ROUNDS}\nTies: {tie_percent}"
+# )
